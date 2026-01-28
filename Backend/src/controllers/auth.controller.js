71fd4a2abfbfc690b55032fa9e50caf7e5cb54f0
anirudhgautam12 <gautamanirudh372@gmail.com>
2026-01-28 17:31:32 +0530

    login refactoring

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -67,6 +67,9 @@ export const signup =async (req,res)=>{
 
 export const login = async (req,res)=>{
     const {email,password}=req.body
+    if(!email || !password){
+        return res.status(400).json({message:"All fields are required"})
+    }
     
     try {
         if(!email || !password){
