c330709829780d3d5f8bf4ddc662a6882c818d43
anirudhgautam12 <gautamanirudh372@gmail.com>
2026-01-28 16:15:33 +0530

    signup section ocde refactoring

--- Backend/src/lib/db.js
+++ Backend/src/lib/db.js
@@ -2,6 +2,10 @@ import mongooose from "mongoose";
 
 export const connectDB=async (url)=>{
     try {
+        const {MONGO_URL}=process.env;
+        if(!MONGO_URL){
+            throw new Error("MONGO_URL is not defined in environment variables");
+        }
         const conn = await mongooose.connect(process.env.MONGO_URL);
         console.log("Database connected successfully : "+conn.connection.host);
     } catch (error) {
