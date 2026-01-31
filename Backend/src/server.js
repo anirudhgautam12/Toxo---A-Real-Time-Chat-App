a6d4abc2877a2e665a30f44ae779a3937bff766e
anirudhgautam12 <gautamanirudh372@gmail.com>
2026-01-31 20:27:56 +0530

    signup page on frontend implemented

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -5,7 +5,7 @@ import messageRoutes from "./routes/message.route.js";
 import Path from "path";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 
 const app = express();
 const __dirname = Path.resolve();
@@ -13,6 +13,7 @@ const __dirname = Path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json()); //req.body
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 app.use(cookieParser());
 
 app.use("/api/auth",authRoutes);
